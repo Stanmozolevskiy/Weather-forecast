89e6077457379c1afac112865933586f2041d4f8
stanmozolevskiy <stanmozolevskiy@gmail.com>
2021-12-13 09:20:39 -0500

    `deploy`

--- src/app/weather/data.service.ts
+++ src/app/weather/data.service.ts
@@ -7,12 +7,13 @@ import { Weather } from '../weather';
   providedIn: 'root'
 })
 export class DataService {
-baseurl:string = " http://api.weatherstack.com/current?";
+baseurl:string = "https://api.weatherstack.com/current?";
 state:string ="US";
+key:string= "access_key=435785ba3893e6cc6bfdba4d894252f0"
 
   constructor(private httpClient: HttpClient) { }
 
-  get(query:string, key: string):Observable<Weather>{
-    return this.httpClient.get<Weather>(`${this.baseurl}${key}&query=${query}&units=f`)
+  get(query:string):Observable<Weather>{
+    return this.httpClient.get<Weather>(`${this.baseurl}${this.key}&query=${query}&units=f&forecast_days=7`)
   }
 }
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -18,36 +18,21 @@ export class WeatherComponent implements OnInit {
   constructor(private dataService: DataService, private fB: FormBuilder) { }
   
   ngOnInit(): void {
-    this.getTheData();
+    this.dataService.get(this.defaultCity).subscribe(res=>{
+      this.weather = res;
+    });
     this.cityForm = this.fB.group({
       city:'',
     });
   }
   
   onSubmit(){
-    this.defaultCity =this.cityForm.get('city')?.value;
-    this.getTheData();
+    this.dataService.get(this.cityForm.get('city')?.value).subscribe(res=>{
+      this.weather = res;
+    });
     this.cityForm.reset(); 
   }
   
-  //need this func to try 2 different api keys 
-  //because the free version alows you to do 250 calls
-  private getTheData(){
-    const key1:string = "access_key=435785ba3893e6cc6bfdba4d894252f0";
-    const key2:string = "access_key=8e443c423702739b3e03b494da5ffc0a";
-    return  this.dataService.get(this.defaultCity, key1).subscribe(res=>{
-      if(res.error.info){
-        this.dataService.get(this.defaultCity, key2).subscribe(res=>{
-           this.weather = res;
-           if(res.error.info){
-             this.isAPILimitReached = true;
-              return;
-           }
-          }) 
-        }
-           else
-            this.weather = res;
-    });
-  }
+ 
   private defaultCity: string ='Portsmouth nh';
 }
